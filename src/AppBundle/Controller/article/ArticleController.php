baca10ceeb6bcdd608e80034fd4824c5af9d13fb
Hamzouz <hamz.m@hotmail.fr>
2016-03-30 10:59:54 +0200

    Ajout test articles + tag

--- src/AppBundle/Controller/article/ArticleController.php
+++ src/AppBundle/Controller/article/ArticleController.php
@@ -4,21 +4,28 @@ namespace AppBundle\Controller\Article;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller
 {
-    public function listAction()
-    {
-
-    }
 
     /**
-     * @Route("/{id")
+     * @Route("/{id}", requirements = {"id" = "\d+"})
      *
      * @param $id
      */
-    public function showAction($id)
+    public function showAction($id, Request $request)
     {
+        $tag = $request->query->get('tag');
+        return new Response('Article avec l\'id '.$id.' avec le tag: '.$tag);
+    }
 
+    /**
+     * @Route("/list")
+     */
+    public function listAction()
+    {
+        return new Response('List of articles');
     }
 }
